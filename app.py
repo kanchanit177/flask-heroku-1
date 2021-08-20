6054673f2b8f63b6cef921e76afbaf9cd33983f0
kanchanit177 <87840164+kanchanit177@users.noreply.github.com>
2021-08-20 09:22:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name ():
-     return "กานต์ชนิต แก้วศรีงาม<br>เลขที่ 6 ม.4/10"
+     return "กานต์ชนิต แก้วศรีงาม <br> เลขที่ 6 ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
